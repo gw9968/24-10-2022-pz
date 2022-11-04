b9b4a34718ae58d7c2819a812b3a8ef8d6db99cc
gw9968 <gw9968ans@gmail.com>
2022-11-04 10:38:41 +0100

    zad 1 - obliczanie miejsca zerowego funkcji liniowej

---
+++ 24-10-2022pz/24-10-2022pz.cpp
@@ -0,0 +1,24 @@
+﻿// 24-10-2022pz.cpp : This file contains the 'main' function. Program execution begins and ends there.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Napisz program obliczania miejsc zerowych funkcji liniowej postaci ax + b gdzie a≠0(wer.1.0)
+
+    double a, b, mz = 0;
+
+    cout << "y = ax + b";
+    cout << "Podaj a: ";
+
+    cin >> a;
+    cout << "Podaj b: ";
+    cin >> b;
+
+    mz = -b / a;
+    cout << "Miejsce zerowe " << mz;
+
+
+    return 0;
+}
+
